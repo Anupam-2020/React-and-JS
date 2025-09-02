4ab89065c5281f667d0892f87d4edf67357e3fab
Anupam-2020 <anupamanand.696@gmail.com>
2025-09-03 02:20:20 +0530

    integrated mongodb in express project

--- backend/9.Express_project/controllers/contactController.js
+++ backend/9.Express_project/controllers/contactController.js
@@ -1,58 +1,95 @@
 const asyncHandler = require('express-async-handler');
+const Contact = require('../models/contactModel');
 
 //@desc Get all contacts.
 //@route GET /api/contacts
 //@access Public
 const getContact = asyncHandler(async (req, resp) => {
-    resp.status(200).json({
-        message: "Get all contacts"
-    })
+    const contacts = await Contact.find();
+    resp.status(200).json(contacts);
+    // resp.status(200).json({
+    //     message: "Get all contacts"
+    // })
 })
 
 //@desc Create new contact.
 //@route POST /api/contacts
 //@access Public
 const createContact = asyncHandler(async (req, resp) => {
-    const {name, email} = req.body;
-    if(!name || !email) {
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone) {
         resp.status(400)
-        throw new Error("Name and email are required");
+        throw new Error("Name, email and phone are required");
     }
-    resp.status(201).json({
-        message: "Create Contact from controller"
+
+    const contact = await Contact.create({
+        name, email, phone
     })
+    resp.status(201).json(contact);
+    // resp.status(201).json({
+    //     message: "Create Contact from controller"
+    // })
 })
 
 //@desc Update contact.
 //@route PUT /api/contacts/:id
 //@access Public
 const updateContact = asyncHandler(async (req, resp, next) => {
-    const id = req.params.id;
-    resp.status(200)
-    resp.json({
-        message: `Update contact with id ${id}`
-    })
+    const contact = await Contact.findById(req.params.id);
+
+    if(contact === undefined || contact === null) {
+        resp.status(404);
+        throw new Error("Contact not found");
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    )
+    resp.status(200).json(updatedContact);
+
+    // resp.status(200).json(contact);
+    // const id = req.params.id;
+    // resp.status(200).json({
+    //     message: `Update contact with id ${id}`
+    // })
 })
 
 //@desc Delete contact.
 //@route DELETE /api/contacts/:id
 //@access Public
 const deleteContact = asyncHandler(async (req, resp, next) => {
-    const id = req.params.id;
-    resp.status(200)
-    resp.json({
-        message: `Delete contact with id ${id}`
-    })
+    const contact = await Contact.findById(req.params.id);
+    if(contact === undefined || contact === null) {
+        resp.status(404);
+        throw new Error("Contact not found");
+    }
+
+    await Contact.deleteOne({_id: req.params.id});
+    resp.status(200).json(contact);
+
+    // const id = req.params.id;
+    // resp.status(200)
+    // resp.json({
+    //     message: `Delete contact with id ${id}`
+    // })
 })
 
 //@desc Get single contact.
 //@route GET /api/contacts/:id
 //@access Public
 const getSingleContact = asyncHandler(async (req, resp) => {
-    const id = req.params.id;
-    resp.status(200).json({
-        message: `Get contact with id ${id}`
-    })
+    const contact = await Contact.findById(req.params.id);
+    console.log(contact);
+    if(contact === undefined || contact === null) {
+        resp.status(404);
+        throw new Error("Contact not found");
+    }
+    resp.status(200).json(contact);
+    // const id = req.params.id;
+    // resp.status(200).json({
+    //     message: `Get contact with id ${id}`
+    // })
 })
 
 module.exports = {
--- backend/9.Express_project/server.js
+++ backend/9.Express_project/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/dbConnection');
 dotenv.config();
+
 const app = express();
 const router = require('./routes/contactRoute');
 const errorHandler = require('./middleware/errorhandler');
 
 const port = process.env.PORT || 5001;
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
